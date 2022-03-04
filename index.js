4f9184158045312716b6ce5d647520ecd3bc0aa4
Luis Ibarra <luis9609@gmail.com>
2022-03-04 11:01:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Write your code here!
 main.remove();
 var newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Luis is the champion";
+
+
 // I'm not sure what to do from here!
